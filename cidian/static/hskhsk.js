312ce55521980e58e199e75eb55c75675807caf3
Alan Davies <alan@galax.xyz>
2020-05-31 19:51:09 -0400

    Added cidian ap as served on hskhsk.pythonanywhere.com

---
+++ cidian/static/hskhsk.js
@@ -0,0 +1,13 @@
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-39955029-3', 'pythonanywhere.com');
+ga('send', 'pageview');
+
+function toggle_visibility(id) {
+    var e = document.getElementById(id);
+    e.style.display = ((e.style.display != 'block') ? 'block' : 'none');
+}
+
