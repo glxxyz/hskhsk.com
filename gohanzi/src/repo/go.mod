000ae6db424483a8c16d8b47c31fb6ba08067c71
Alan Davies <alan@galax.xyz>
2021-06-26 14:18:00 -0400

    Moved gohanzi to its own repository

--- gohanzi/src/repo/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/glxxyz/hskhsk.com/gohanzi/repo
-
-go 1.14
--- gohanzi/static/hskhsk.js
+++
@@ -1,5 +0,0 @@
-function toggle_visibility(id) {
-    const e = document.getElementById(id);
-    e.style.display = (e.style.display !== 'block' ? 'block' : 'none');
-}
-
